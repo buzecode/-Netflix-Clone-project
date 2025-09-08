f570b9e2bcc4da3bf30169dc7e92df9ca93416bd
buzecode <bizuayehutakele195@gmail.com>
2025-09-08 19:50:19 -0400

    axios and request

---
+++ react-netflix/src/utlis/requests.jsx
@@ -0,0 +1,15 @@
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const requests = {
+    fetchTrending :'/trending/all/week?api_key=${API_KYE}&languge=en-US',
+    fetchNetflixOriginals:'/discover/tv?api_key=${API_KEY}&with_networks=213',
+    fetchTopRatedMovies:'/movie/top_rated?api_key=${API_KEY}&languge=en-US',
+    fetchActionMovies: '/discover/movie?api__key=${API_KYE}&with_genres=28',
+    fetchComedyMovies:'/discover/movie?api__key=${API_KYE}&with_genres=35',
+    fetchHorrorMovies:'/discover/movie?api__key=${API_KYE}&with_genres=27',
+    fetchRomanceMovies:'/discover/movie?api__key=${API_KYE}&with_genres=10749',
+    fetchDocumentaries:'/discover/movie?api_key=${API_KEY}&with_genres=99',
+    fetchTvShow:'tv/popular?api_key=&{API_KEY}&languge=en-US&page=1',
+    fetchDrama:'/discover/movie?api_key=${API_KEY}&with_genres=18',
+    fetchMusic:'/discover/movie?api_key=${API_KYE}&with_genres=10402'
+}
